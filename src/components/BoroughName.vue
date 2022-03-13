40a8854de0176e895ca293af0aa359a1e9c57692
Sean Kelliher <sbkelliher@icloud.com>
2022-03-13 14:09:46 -0400

    Add v-if. Adds 'the' for Bronx.

--- src/components/BoroughName.vue
+++ src/components/BoroughName.vue
@@ -3,6 +3,7 @@
         <h2>{{ name }}</h2>
         <p>
             Year-round indoor and summer outdoor public swimming pools in
+            <span v-if="name === 'Bronx'">the</span>
             {{ name }}.
             <span class="highlight">
                 Pool dimensions: L x W x D (in feet).
