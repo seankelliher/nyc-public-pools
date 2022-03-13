a378dd9239b29db382e16d81e0b96e6869ccf2b2
Sean Kelliher <sbkelliher@icloud.com>
2022-03-13 01:43:59 -0500

    Add BoroughName, FeesSchedule, and PoolCard components.

---
+++ src/components/BoroughName.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="borough">
+        <h2>{{ name }}</h2>
+        <p>
+            Year-round indoor and summer outdoor public swimming pools in
+            {{ name }}.
+            <span class="highlight">
+                Pool dimensions: L x W x D (in feet).
+            </span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
---
+++ src/components/PoolCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="pool" v-for="(pool, index) in pools" v-bind:key="index">
+        <h3>{{ pool.name }}</h3>
+        <p>{{ pool.street }}</p>
+        <p>{{ pool.borough }}, NY {{ pool.zip }}</p>
+        <p>Telephone: {{ pool.phone }}</p>
+        <p v-if="pool.variants[0]">
+            {{ pool.variants[0].desc }}: {{ pool.variants[0].length }} x
+            {{ pool.variants[0].width }} x {{ pool.variants[0].depth }}.
+        </p>
+        <p v-if="pool.variants[1]">
+            {{ pool.variants[1].desc }}: {{ pool.variants[1].length }} x
+            {{ pool.variants[1].width }} x {{ pool.variants[1].depth }}.
+        </p>
+        <p v-if="pool.variants[2]">
+            {{ pool.variants[2].desc }}: {{ pool.variants[2].length }} x
+            {{ pool.variants[2].width }} x {{ pool.variants[2].depth }}.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PoolCard",
+    props: {
+        pools: {
+            type: Array,
+            required: true,
+        },
+    },
+};
+</script>
