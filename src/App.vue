6b715a4aa4a43356a218b26894ff6d1344b5c11f
Sean Kelliher <sbkelliher@icloud.com>
2022-03-13 01:32:21 -0500

    Add header and additional router links.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
+    <header>
+        <h1>NYC Public Pools</h1>
+        <!--<p>Public swimming pools wihin the five boroughs of New York City.</p>-->
+    </header>
     <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <button><router-link to="/">Home</router-link></button>
+        <button><router-link to="/bronx">BX</router-link></button>
+        <button><router-link to="/brooklyn">BK</router-link></button>
+        <button><router-link to="/manhattan">MN</router-link></button>
+        <button><router-link to="/queens">QN</router-link></button>
+        <button>
+            <router-link to="/staten-island">SI</router-link>
+        </button>
     </nav>
     <router-view />
 </template>
 
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-nav {
-    padding: 30px;
-}
-
-nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-    color: #42b983;
-}
+@import "./assets/style.css";
 </style>
