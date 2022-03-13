188bf3a6783835eac47d5f6a2cd082918a364f9e
Sean Kelliher <sbkelliher@icloud.com>
2022-03-13 01:42:02 -0500

    Add section tag and FeesSchedule component within it.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
-    </div>
+    <section>
+        <FeesSchedule />
+    </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import FeesSchedule from "@/components/FeesSchedule.vue";
 
 export default {
     name: "HomeView",
     components: {
-        HelloWorld,
+        FeesSchedule,
     },
 };
 </script>
