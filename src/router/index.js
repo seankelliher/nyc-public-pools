19810f2981a9fde93e6e7d9f1a27386013c0ded5
Sean Kelliher <sbkelliher@icloud.com>
2022-03-13 14:08:37 -0400

    Tidy router links. Add nprogress package, NotFound component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,72 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import BronxPools from "../views/BronxPools.vue";
+import BrooklynPools from "../views/BrooklynPools.vue";
+import ManhattanPools from "../views/ManhattanPools.vue";
+import QueensPools from "../views/QueensPools.vue";
+import StatenPools from "../views/StatenPools.vue";
+import NotFound from "../views/NotFound.vue";
+import NProgress from "nprogress";
 
 const routes = [
     {
         path: "/",
         name: "home",
         component: HomeView,
+        // route level code-splitting
+        // this generates a separate chunk (home.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        //component: () => import("../views/HomeView.vue"),
     },
     {
         path: "/about",
-        name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import("../views/AboutView.vue"),
+        name: "AboutView",
+        component: AboutView,
     },
     {
         path: "/bronx",
-        name: "bronx",
-        component: () => import("../views/BronxPools.vue"),
+        name: "BronxPools",
+        component: BronxPools,
     },
     {
         path: "/brooklyn",
-        name: "brooklyn",
-        component: () => import("../views/BrooklynPools.vue"),
+        name: "BrooklynPools",
+        component: BrooklynPools,
     },
     {
         path: "/manhattan",
-        name: "manhattan",
-        component: () => import("../views/ManhattanPools.vue"),
+        name: "ManhattanPools",
+        component: ManhattanPools,
     },
     {
         path: "/queens",
-        name: "queens",
-        component: () => import("../views/QueensPools.vue"),
+        name: "QueensPools",
+        component: QueensPools,
     },
     {
         path: "/staten-island",
-        name: "statenIsland",
-        component: () => import("../views/StatenPools.vue"),
+        name: "StatenPools",
+        component: StatenPools,
+    },
+    {
+        path: "/:catchAll(.*)",
+        name: "NotFound",
+        component: NotFound,
     },
 ];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
+});
+
+router.beforeEach(() => {
+    NProgress.start();
+});
+
+router.afterEach(() => {
+    NProgress.done();
 });
 
 export default router;
