9d9f51f617d26ba38f529a200bbdb1aa3eb68db9
Sean Kelliher <sbkelliher@icloud.com>
2022-03-15 16:41:23 -0400

    Add the component PoolPhoto.vue.

---
+++ src/components/PoolPhoto.vue
@@ -0,0 +1,26 @@
+<template>
+    <figure>
+        <img :src="require(`@/assets/images/${src}.jpg`)" :alt="alt" />
+        <figcaption>{{ caption }}</figcaption>
+    </figure>
+</template>
+
+<script>
+export default {
+    name: "PoolPhoto",
+    props: {
+        src: {
+            type: String,
+            required: true,
+        },
+        alt: {
+            type: String,
+            required: true,
+        },
+        caption: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
