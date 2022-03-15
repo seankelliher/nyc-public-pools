d85d214107867c5a64914a48ea21f420eeec607a
Sean Kelliher <sbkelliher@icloud.com>
2022-03-15 16:28:14 -0400

    Move the header and nav elements to their own component - HeaderNav.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
-    <header>
-        <h1>NYC Public Pools</h1>
-        <!--<p>Public swimming pools wihin the five boroughs of New York City.</p>-->
-    </header>
-    <nav>
-        <button><router-link to="/">Home</router-link></button>
-        <button><router-link to="/bronx">BX</router-link></button>
-        <button><router-link to="/brooklyn">BK</router-link></button>
-        <button><router-link to="/manhattan">MN</router-link></button>
-        <button><router-link to="/queens">QN</router-link></button>
-        <button>
-            <router-link to="/staten-island">SI</router-link>
-        </button>
-    </nav>
+    <HeaderNav />
     <router-view />
+    <FooterPart />
 </template>
+
+<script>
+// @ is an alias to /src
+import HeaderNav from "@/components/HeaderNav.vue";
+import FooterPart from "@/components/FooterPart.vue";
+
+export default {
+    name: "App",
+    components: {
+        HeaderNav,
+        FooterPart,
+    },
+};
+</script>
 
 <style>
 @import "./assets/style.css";
