e0037830dbcd10c4562e6072bcf41add89fffede
Sean Kelliher <sbkelliher@icloud.com>
2022-03-15 16:29:46 -0400

    Add the component FooterPart.vue.

---
+++ src/components/FooterPart.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer>
+        <h3>Questions</h3>
+        <p>Content coming soon.</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "FooterPart",
+};
+</script>
