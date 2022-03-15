4bc8a769c78eca356f7a6db8942a2c934abfe1f1
Sean Kelliher <sbkelliher@icloud.com>
2022-03-15 16:38:38 -0400

    Put pool dimensions in 'ul' list.

--- src/components/PoolCard.vue
+++ src/components/PoolCard.vue
@@ -1,21 +1,30 @@
 <template>
-    <div class="pool" v-for="(pool, index) in pools" v-bind:key="index">
+    <h3>Indoor &amp; Outdoor Pools</h3>
+    <div class="pool" v-for="(pool, index) in pools" :key="index">
         <h3>{{ pool.name }}</h3>
-        <p>{{ pool.street }}</p>
-        <p>{{ pool.borough }}, NY {{ pool.zip }}</p>
-        <p>Telephone: {{ pool.phone }}</p>
-        <p v-if="pool.variants[0]">
-            {{ pool.variants[0].desc }}: {{ pool.variants[0].length }} x
-            {{ pool.variants[0].width }} x {{ pool.variants[0].depth }}.
+        <p>
+            {{ pool.street }}<br />
+            {{ pool.borough }}, NY {{ pool.zip }}<br />
+            Telephone: {{ pool.phone }}<br />
+            Cross Streets: {{ pool.betweens }}
         </p>
-        <p v-if="pool.variants[1]">
-            {{ pool.variants[1].desc }}: {{ pool.variants[1].length }} x
-            {{ pool.variants[1].width }} x {{ pool.variants[1].depth }}.
-        </p>
-        <p v-if="pool.variants[2]">
-            {{ pool.variants[2].desc }}: {{ pool.variants[2].length }} x
-            {{ pool.variants[2].width }} x {{ pool.variants[2].depth }}.
-        </p>
+        <ul>
+            <li v-if="pool.variants[0]">
+                {{ pool.variants[0].desc }}: {{ pool.variants[0].length }}L x
+                {{ pool.variants[0].width }}W x {{ pool.variants[0].depth }}D
+                feet.
+            </li>
+            <li v-if="pool.variants[1]">
+                {{ pool.variants[1].desc }}: {{ pool.variants[1].length }}L x
+                {{ pool.variants[1].width }}W x {{ pool.variants[1].depth }}D
+                feet.
+            </li>
+            <li v-if="pool.variants[2]">
+                {{ pool.variants[2].desc }}: {{ pool.variants[2].length }}L x
+                {{ pool.variants[2].width }}W x {{ pool.variants[2].depth }}D
+                feet.
+            </li>
+        </ul>
     </div>
 </template>
 
