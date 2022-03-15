4fb0a6205f7928117060eab75b13c55ffb7e9ebf
Sean Kelliher <sbkelliher@icloud.com>
2022-03-15 16:40:29 -0400

    Install the div class 'pool' that puts content in grey boxes.

--- src/components/FeesSchedule.vue
+++ src/components/FeesSchedule.vue
@@ -1,25 +1,31 @@
 <template>
-    <h2>About</h2>
-    <p>
-        The New York City Department of Parks and Recreation operates indoor and
-        outdoor pool that are are available to the public.
-    </p>
-    <h2>Indoor Pools</h2>
-    <p>
-        Indoor public pools are <strong>open year-round</strong> and located
-        within recreation centers. To use these pools, you purchase a membership
-        to the recreation center. An annual membership costs $150 for adults and
-        $25 for seniors, and is free for anyone under the age of 18. Schedules
-        at indoor pools vary. See each pool for details.
-    </p>
-    <h2>Outdoor Pools</h2>
-    <p>
-        Outdoor public pools are <strong>open in summer</strong> and generally
-        located within parks and playgrounds. These pools are free. Outdoor pool
-        hours are from 11:00 a.m. through 7:00 p.m. daily, with a break for pool
-        cleaning between 3:00 p.m. and 4:00 p.m. Mini pools are closed on
-        Tuesdays.
-    </p>
+    <div class="pool">
+        <h3>About NYC Parks</h3>
+        <p>
+            The New York City Department of Parks and Recreation operates indoor
+            and outdoor pool that are are available to the public.
+        </p>
+    </div>
+    <div class="pool">
+        <h3>Indoor Pools</h3>
+        <p>
+            Indoor public pools are <strong>open year-round</strong> and located
+            within recreation centers. To use these pools, you purchase a
+            membership to the recreation center. An annual membership costs $150
+            for adults and $25 for seniors, and is free for anyone under the age
+            of 18. Schedules at indoor pools vary. See each pool for details.
+        </p>
+    </div>
+    <div class="pool">
+        <h3>Outdoor Pools</h3>
+        <p>
+            Outdoor public pools are <strong>open in summer</strong> and
+            generally located within parks and playgrounds. These pools are
+            free. Outdoor pool hours are from 11:00 a.m. through 7:00 p.m.
+            daily, with a break for pool cleaning between 3:00 p.m. and 4:00
+            p.m. Mini pools are closed on Tuesdays.
+        </p>
+    </div>
 </template>
 
 <script>
