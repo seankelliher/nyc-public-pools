6af6a83509933449bfea22d5e678bfbdbdd4cc93
Sean Kelliher <sbkelliher@icloud.com>
2022-03-16 16:18:35 -0400

    Add v-if statements to include 'accessible' (wheelchair) icon when applicable.

--- src/components/PoolCard.vue
+++ src/components/PoolCard.vue
@@ -13,16 +13,31 @@
                 {{ pool.variants[0].desc }}: {{ pool.variants[0].length }}L x
                 {{ pool.variants[0].width }}W x {{ pool.variants[0].depth }}D
                 feet.
+                <span v-if="pool.variants[0].accessible">
+                    <img
+                        src="@/assets/images/accessible-forward-black-24dp.svg"
+                    />
+                </span>
             </li>
             <li v-if="pool.variants[1]">
                 {{ pool.variants[1].desc }}: {{ pool.variants[1].length }}L x
                 {{ pool.variants[1].width }}W x {{ pool.variants[1].depth }}D
                 feet.
+                <span v-if="pool.variants[1].accessible">
+                    <img
+                        src="@/assets/images/accessible-forward-black-24dp.svg"
+                    />
+                </span>
             </li>
             <li v-if="pool.variants[2]">
                 {{ pool.variants[2].desc }}: {{ pool.variants[2].length }}L x
                 {{ pool.variants[2].width }}W x {{ pool.variants[2].depth }}D
                 feet.
+                <span v-if="pool.variants[2].accessible">
+                    <img
+                        src="@/assets/images/accessible-forward-black-24dp.svg"
+                    />
+                </span>
             </li>
         </ul>
     </div>
