9fbe58b722c1e112277cee0313e71bdce7bab717
Sean Kelliher <sbkelliher@icloud.com>
2022-03-16 16:16:43 -0400

    Minor text change about cost.

--- src/components/FeesSchedule.vue
+++ src/components/FeesSchedule.vue
@@ -12,8 +12,9 @@
             Indoor public pools are <strong>open year-round</strong> and located
             within recreation centers. To use these pools, you purchase a
             membership to the recreation center. An annual membership costs $150
-            for adults and $25 for seniors, and is free for anyone under the age
-            of 18. Schedules at indoor pools vary. See each pool for details.
+            for adults and $25 for seniors (ages 62 and older), and is free for
+            anyone under the age of 18. Schedules at indoor pools vary. See each
+            pool for details.
         </p>
     </div>
     <div class="pool">
