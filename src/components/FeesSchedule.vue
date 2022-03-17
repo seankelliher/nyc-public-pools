73fca8ab04e8ff3e90300d6530efeae5c8290a44
Sean Kelliher <sbkelliher@icloud.com>
2022-03-17 17:51:44 -0400

    Added open date for 2022 outdoor pools.

--- src/components/FeesSchedule.vue
+++ src/components/FeesSchedule.vue
@@ -24,7 +24,7 @@
             generally located within parks and playgrounds. These pools are
             free. Outdoor pool hours are from 11:00 a.m. through 7:00 p.m.
             daily, with a break for pool cleaning between 3:00 p.m. and 4:00
-            p.m. Mini pools are closed on Tuesdays.
+            p.m. Mini pools are closed on Tuesdays. Pools re-open June 28, 2022.
         </p>
     </div>
 </template>
