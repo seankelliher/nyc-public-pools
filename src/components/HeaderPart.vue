7015b9bc8ea781874557131aa60cd13a7198a51f
Sean Kelliher <sbkelliher@icloud.com>
2022-03-17 17:40:00 -0400

    Divided HeaderNav.vue into two components: HeaderPart.vue and NavPart.vue.

---
+++ src/components/HeaderPart.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <h1>Public Swimming Pools in NYC</h1>
+        <!--<p>Public swimming pools wihin the five boroughs of New York City.</p>-->
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderPart",
+};
+</script>
---
+++ src/components/NavPart.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <button><router-link to="/">Home</router-link></button>
+        <button><router-link to="/bronx">BX</router-link></button>
+        <button><router-link to="/brooklyn">BK</router-link></button>
+        <button><router-link to="/manhattan">MN</router-link></button>
+        <button><router-link to="/queens">QN</router-link></button>
+        <button>
+            <router-link to="/staten-island">SI</router-link>
+        </button>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavPart",
+};
+</script>
