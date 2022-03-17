9a9b8126175bd8ba21117337460c41f3664d9ada
Sean Kelliher <sbkelliher@icloud.com>
2022-03-17 17:50:30 -0400

    Adding the components: IndoorSummary.vue, OutdoorSummary.vue, and ParksSummary.vue. These will replace FeesSchedule.vue.

---
+++ src/components/IndoorSummary.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="pool">
+        <h3>Indoor Pools</h3>
+        <p>
+            Indoor public pools are <strong>open year-round</strong> and located
+            within recreation centers. To use these pools, you purchase a
+            membership to the recreation center. An annual membership costs $150
+            for adults and $25 for seniors (ages 62 and older), and is free for
+            anyone under the age of 18. Schedules at indoor pools vary. See each
+            pool for details.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IndoorSummary",
+};
+</script>
---
+++ src/components/ParksSummary.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pool">
+        <h3>About NYC Parks</h3>
+        <p>
+            The New York City Department of Parks and Recreation operates indoor
+            and outdoor pool that are are available to the public.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ParksSummary",
+};
+</script>
