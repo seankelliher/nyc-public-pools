2097a7a3d9ada0cf05034e8bdf6e491789a97550
Sean Kelliher <sbkelliher@icloud.com>
2022-03-20 09:41:02 -0400

    Add the component FeesSchedules.vue.

---
+++ src/components/FeesSchedules.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="borough">
+        <h3>Indoor &amp; Outdoor Pools</h3>
+        <button class="regular" @click="visible = !visible">
+            fees &amp; schedules
+        </button>
+        <Transition>
+            <p v-show="visible">
+                See our <a href="/">homepage</a> for information on pool fees
+                &amp; schedules.
+            </p>
+        </Transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FeesSchedules",
+    data() {
+        return {
+            visible: false,
+        };
+    },
+};
+</script>
+
+<style scoped>
+p {
+    line-height: 1.2;
+    margin-top: 0;
+}
+</style>
