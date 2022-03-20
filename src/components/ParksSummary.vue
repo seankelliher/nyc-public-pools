8d57871ad6613e62b0f582a8a889dc9a3396d3a8
Sean Kelliher <sbkelliher@icloud.com>
2022-03-20 13:39:28 -0400

    Text changes to the 'NYC Parks' section.

--- src/components/ParksSummary.vue
+++ src/components/ParksSummary.vue
@@ -2,8 +2,9 @@
     <div class="pool">
         <h3>About NYC Parks</h3>
         <p>
-            The New York City Department of Parks and Recreation operates indoor
-            and outdoor pool that are are available to the public.
+            The New York City Department of Parks and Recreation operates pool
+            in all five boroughs. There are both indoor and outdoor pool and all
+            are available to the public.
         </p>
     </div>
 </template>
