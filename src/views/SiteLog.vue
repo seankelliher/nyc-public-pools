b5d0291794af5f115be3faca6b83992691620fd9
Sean Kelliher <sbkelliher@icloud.com>
2022-03-20 13:41:06 -0400

    Add SiteLog.vue. Scaffolding for now.

---
+++ src/views/SiteLog.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <h2>Site Log</h2>
+        <p>Under development.</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "SiteLog",
+};
+</script>
