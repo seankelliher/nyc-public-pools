e5570a673666cc9661d205b1906b8654a475b2a0
Sean Kelliher <sbkelliher@icloud.com>
2022-03-20 09:34:10 -0400

    Add the 'nav' class to buttons.

--- src/components/NavPart.vue
+++ src/components/NavPart.vue
@@ -1,11 +1,15 @@
 <template>
     <nav>
-        <button><router-link to="/">Home</router-link></button>
-        <button><router-link to="/bronx">BX</router-link></button>
-        <button><router-link to="/brooklyn">BK</router-link></button>
-        <button><router-link to="/manhattan">MN</router-link></button>
-        <button><router-link to="/queens">QN</router-link></button>
-        <button>
+        <button class="nav"><router-link to="/">Home</router-link></button>
+        <button class="nav"><router-link to="/bronx">BX</router-link></button>
+        <button class="nav">
+            <router-link to="/brooklyn">BK</router-link>
+        </button>
+        <button class="nav">
+            <router-link to="/manhattan">MN</router-link>
+        </button>
+        <button class="nav"><router-link to="/queens">QN</router-link></button>
+        <button class="nav">
             <router-link to="/staten-island">SI</router-link>
         </button>
     </nav>
