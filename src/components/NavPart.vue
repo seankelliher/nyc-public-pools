3c607cc44fbe45071146e8015740d537d4b032e6
Sean Kelliher <sbkelliher@icloud.com>
2022-05-10 22:48:02 -0400

    Add 'goToPage' method and IDs on buttons.

--- src/components/NavPart.vue
+++ src/components/NavPart.vue
@@ -1,15 +1,18 @@
 <template>
     <nav>
-        <!--<button class="nav"><router-link to="/">Home</router-link></button>-->
-        <button class="nav"><router-link to="/bronx">BX</router-link></button>
-        <button class="nav">
+        <button @click="goToPage" class="nav" id="btn-bx">
+            <router-link to="/bronx">BX</router-link>
+        </button>
+        <button @click="goToPage" class="nav" id="btn-bk">
             <router-link to="/brooklyn">BK</router-link>
         </button>
-        <button class="nav">
+        <button @click="goToPage" class="nav" id="btn-mn">
             <router-link to="/manhattan">MN</router-link>
         </button>
-        <button class="nav"><router-link to="/queens">QN</router-link></button>
-        <button class="nav">
+        <button @click="goToPage" class="nav" id="btn-qn">
+            <router-link to="/queens">QN</router-link>
+        </button>
+        <button @click="goToPage" class="nav" id="btn-si">
             <router-link to="/staten-island">SI</router-link>
         </button>
     </nav>
@@ -18,5 +21,27 @@
 <script>
 export default {
     name: "NavPart",
+    methods: {
+        goToPage(event) {
+            if (event.target.id === "btn-bx") {
+                this.$router.push("/bronx");
+            }
+            if (event.target.id === "btn-bk") {
+                this.$router.push("/brooklyn");
+            }
+            if (event.target.id === "btn-mn") {
+                this.$router.push("/manhattan");
+            }
+            if (event.target.id === "btn-qn") {
+                this.$router.push("/queens");
+            }
+            if (event.target.id === "btn-si") {
+                this.$router.push("/staten-island");
+            }
+        },
+    },
 };
 </script>
+<!--Note: there is some repetition in the above code. I'll explain.-->
+<!--goToPage method & btn IDS make the whole btn (not only txt) clickable.-->
+<!--The <router-link> lets us use Vue's ".router-link-exact-active" class.-->
