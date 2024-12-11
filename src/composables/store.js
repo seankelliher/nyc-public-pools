202dbede2d03d12724ea213e84f3d46b19a8f702
Sean Kelliher <sbkelliher@icloud.com>
2024-12-11 00:55:34 -0500

    Add 'store' file for shared variables and functions.

---
+++ src/composables/store.js
@@ -0,0 +1,141 @@
+import { reactive } from "vue";
+import { pools } from "../data/pools.js";
+
+export const store = reactive({
+    searchTerm: "",
+    borough: "",
+    locationType: "",
+    swimType: "",
+    selects:[],
+    commons: [],
+    intro: true,
+    showSearch: false,
+    noResults: false,
+    fullPool: 0,
+    fullPoolName: "",
+    fullPoolDisplayed: false,
+    resetMessages: function() {
+        store.commons.length = 0;
+        store.borough = "";
+        store.locationType = "",
+        store.swimType = "",
+        store.noResults = false;
+        store.searchTerm = "";
+        store.showSearch = false;
+        store.intro = true;
+        store.fullPoolDisplayed = false;
+        store.fullPoolName = "";
+    },
+    displayMessages: function() {
+        if (store.commons.length === 0) {
+            store.noResults = true;
+            store.intro = false;
+        } else {
+            store.noResults = false;
+            store.intro = false;
+        }
+    },
+    processInputs: function() {
+        // Reset the selects array.
+        store.selects.length = 0;
+        store.searchTerm = "";
+        store.showSearch = false;
+        store.fullPoolDisplayed = false;
+        store.fullPoolName = "";
+
+        // Boroughs - group and push pools from selected borough.
+        if (store.borough !== "") {
+            const byBorough = Object.groupBy(pools, ({ borough }) => borough);
+            const boroughSelects = [];
+            for (const item in byBorough[store.borough]) {
+                boroughSelects.push(byBorough[store.borough][item].locId);
+            }
+            store.selects.push(boroughSelects);
+        }
+
+        // Location Types - group and push pools from selected type.
+        if (store.locationType !== "") {
+            const locationTypeSelects = [];
+
+            if (store.locationType === "indoor") {
+                const indoor = Object.groupBy(pools, ({ indoor }) => indoor);
+                for (const item in indoor.true) {
+                    locationTypeSelects.push(indoor.true[item].locId);
+                }
+                store.selects.push(locationTypeSelects);
+            }
+            
+            if (store.locationType === "outdoor") {
+                const outdoor = Object.groupBy(pools, ({ outdoor }) => outdoor);
+                for (const item in outdoor.true) {
+                    locationTypeSelects.push(outdoor.true[item].locId);
+                }
+                store.selects.push(locationTypeSelects);
+            }
+        }
+
+        // Swim Types - group and push pools from selected type.
+        if (store.swimType !== "") {
+            const swimTypeSelects = [];
+
+            if (store.swimType === "diving") {
+                const diving = Object.groupBy(pools, ({ diving }) => diving);
+                for (const item in diving.true) {
+                    swimTypeSelects.push(diving.true[item].locId);
+                }
+                store.selects.push(swimTypeSelects);
+            }
+            
+            if (store.swimType === "swimming") {
+                const swimming = Object.groupBy(pools, ({ swimming }) => swimming);
+                for (const item in swimming.true) {
+                    swimTypeSelects.push(swimming.true[item].locId);
+                }
+                store.selects.push(swimTypeSelects);
+            }
+
+            if (store.swimType === "wading") {
+                const wading = Object.groupBy(pools, ({ wading }) => wading);
+                for (const item in wading.true) {
+                    swimTypeSelects.push(wading.true[item].locId);
+                }
+                store.selects.push(swimTypeSelects);
+            }
+        }
+        // Filter arrays within selects array to find common senators.
+        store.commons = store.selects.reduce((p,c) => p.filter(e => c.includes(e)));
+
+        // Display matching messages in "criteria bar."
+        store.displayMessages();
+    },
+    runSearch: function() {
+        store.showSearch = true;
+        store.commons.length = 0;
+        pools.map(function (pool) {
+            const poolNameLc = pool.name.toLowerCase();
+            const searchTermLc = store.searchTerm.toLowerCase();
+            if (poolNameLc.includes(searchTermLc)) {
+                store.commons.push(pool.locId);
+            } else {
+                store.displayMessages(); 
+            }
+        });
+    },
+    updateBorough: function(bor) {
+        store.borough = bor;
+        store.processInputs();
+    },
+    updateLocationType: function(loc) {
+        store.locationType = loc;
+        store.processInputs();
+    },
+    updateSwimType: function(swim) {
+        store.swimType = swim;
+        store.processInputs();
+    },
+    updateFullPool: function(index) {
+        store.fullPool = index;
+        store.fullPoolDisplayed = true;
+        store.fullPoolName = pools[index].name;
+    }
+});
