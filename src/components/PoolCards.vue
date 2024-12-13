3803ed415f7ba6d419431c48a252ec5f2167eee8
Sean Kelliher <sbkelliher@icloud.com>
2024-12-13 12:09:46 -0500

    Add inner-main class and arrows.

--- src/components/PoolCards.vue
+++ src/components/PoolCards.vue
@@ -13,27 +13,37 @@ function chooseFullPool(pid) {
 
 <template>
     <main>
-        <template v-for="pool in pools" :key="pool.locId">
-            <div
-                v-if="store.commons.includes(pool.locId)"
-                class="pool-card"
-                tabindex="0"
-                @click="chooseFullPool(pool.locId)"
-                @keyup.enter="chooseFullPool(pool.locId)"
-                role="link"
-            >
-                <dl class="pool-card-list">
-                    <dt
-                        class="roboto-flex seven"
-                        :class="{active: store.fullPoolName === pool.name}"
-                    >
-                        {{ pool.name }}
-                    </dt>
-                    <dd class="roboto-flex four">{{ pool.neighborhood }}</dd>
-                    <dd class="roboto-flex four">{{ pool.borough }}</dd>
-                </dl>
-            </div>
-        </template>
+        <figure class="icon-forward">
+            <img src="/images/arrow-forward-ios-24.svg" alt="icon move forward">
+            <figcaption hidden>forward arrow</figcaption>
+        </figure>
+        <div class="inner-main">
+            <template v-for="pool in pools" :key="pool.locId">
+                <div
+                    v-if="store.commons.includes(pool.locId)"
+                    class="pool-card"
+                    tabindex="0"
+                    @click="chooseFullPool(pool.locId)"
+                    @keyup.enter="chooseFullPool(pool.locId)"
+                    role="link"
+                >
+                    <dl class="pool-card-list">
+                        <dt
+                            class="roboto-flex seven"
+                            :class="{active: store.fullPoolName === pool.name}"
+                        >
+                            {{ pool.name }}
+                        </dt>
+                        <dd class="roboto-flex four">{{ pool.neighborhood }}</dd>
+                        <dd class="roboto-flex four">{{ pool.borough }}</dd>
+                    </dl>
+                </div>
+            </template>
+        </div>
+        <figure class="icon-back">
+            <img src="/images/arrow-back-ios-24.svg" alt="icon move forward">
+            <figcaption hidden>forward arrow</figcaption>
+        </figure>
     </main>
 </template>
 
