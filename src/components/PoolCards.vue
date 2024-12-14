d73fefc62c084c5b4e6813e99d029efcce217b3c
Sean Kelliher <sbkelliher@icloud.com>
2024-12-14 11:04:48 -0500

    Add decision tree to show arrows or not.

--- src/components/PoolCards.vue
+++ src/components/PoolCards.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { pools } from "../data/pools.js";
 import { store } from "../composables/store.js";
+import { onMounted, ref } from "vue";
+
+const showBackArrow = ref(false);
+const showForwardArrow = ref(false);
 
 function chooseFullPool(pid) {
     pools.map((pool, index) => {
@@ -9,11 +13,51 @@ function chooseFullPool(pid) {
         }
     });
 }
+
+onMounted(() => {
+    const inner = document.querySelector(".inner-main");
+    inner.addEventListener("mouseover", function () {
+        const scrollLeft = inner.scrollLeft;
+        const scrollWidth = inner.scrollWidth;
+        const clientWidth = inner.clientWidth;
+
+        // DECISION TREE - SHOW ARROWS OR NOT.
+        // If more pools than space, show forward arrow.
+        if (scrollWidth > clientWidth) {
+            showForwardArrow.value = true;
+            showBackArrow.value = false;
+
+            // If user scrolls fully left, hide forward arrow, show back arrow.
+            // If user scrolls fully back, hide back arrow, show forward arrow,
+            // If somewhere in between, show both arrows.
+            if (scrollLeft === 0) {
+                showForwardArrow.value = true;
+                showBackArrow.value = false;
+            } else if (clientWidth + scrollLeft === scrollWidth) {
+                showForwardArrow.value = false;
+                showBackArrow.value = true;
+            } else {
+                showForwardArrow.value = true;
+                showBackArrow.value = true;
+            }
+
+        // If more space than pools, hide arrows.
+        } else if (scrollWidth === clientWidth) {
+            showForwardArrow.value = false;
+            showBackArrow.value = false;
+        }
+    });
+
+    inner.addEventListener("mouseout", function () {
+        showForwardArrow.value = false;
+        showBackArrow.value = false;
+    });
+});
 </script>
 
 <template>
     <main>
-        <figure class="icon-forward">
+        <figure v-if="showForwardArrow" class="icon-forward">
             <img src="/images/arrow-forward-ios-24.svg" alt="icon move forward">
             <figcaption hidden>forward arrow</figcaption>
         </figure>
@@ -40,7 +84,7 @@ function chooseFullPool(pid) {
                 </div>
             </template>
         </div>
-        <figure class="icon-back">
+        <figure v-if="showBackArrow" class="icon-back">
             <img src="/images/arrow-back-ios-24.svg" alt="icon move forward">
             <figcaption hidden>forward arrow</figcaption>
         </figure>
