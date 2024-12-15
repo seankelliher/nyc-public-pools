ab4bbf0c628955cc2451d3a929524445ef3b9961
Sean Kelliher <sbkelliher@icloud.com>
2024-12-14 21:20:51 -0500

    Rework dl lists to satisfy WACG requirements.

--- src/components/FullPool.vue
+++ src/components/FullPool.vue
@@ -8,32 +8,60 @@ import { store } from "../composables/store.js";
         v-if="store.fullPoolDisplayed"
         class="full-pool"
     >
-        <dl class="full-pool-list">
-            <dt class="roboto-flex seven">{{ pools[store.fullPool].name }}</dt>
-            <dd class="roboto-flex four">{{ pools[store.fullPool].street }}</dd>
-            <dd class="roboto-flex four">{{ pools[store.fullPool].borough }}, NY {{ pools[store.fullPool].zip }}</dd>
-            <dd class="roboto-flex four">Phone: {{ pools[store.fullPool].phone }}</dd>
-        </dl>
+        <div class="full-pool-list">
+            <dl>
+                <dt>
+                    <div class="roboto-flex seven">{{ pools[store.fullPool].name }}</div>
+                </dt>
+                <dd>
+                    <div class="roboto-flex four">{{ pools[store.fullPool].street }}</div>
+                </dd>
+                <dd>
+                    <div class="roboto-flex four">{{ pools[store.fullPool].borough }}, NY {{ pools[store.fullPool].zip }}</div>
+                </dd>
+                <dd>
+                    <div class="roboto-flex four">Phone: {{ pools[store.fullPool].phone }}</div>
+                </dd>
+            </dl>
+        </div>
 
-        <dl class="full-pool-list">
-            <dt class="roboto-flex seven">Cross streets &amp; notes:</dt>
-            <dd class="roboto-flex four">{{ pools[store.fullPool].betweens }}. {{ pools[store.fullPool].notes }}</dd>
-        </dl>
+        <div class="full-pool-list">
+            <dl>
+                <dt>
+                    <div class="roboto-flex seven">Cross streets &amp; notes:</div>
+                </dt>
+                <dd>
+                    <div class="roboto-flex four">{{ pools[store.fullPool].betweens }}. {{ pools[store.fullPool].notes }}</div>
+                </dd>
+            </dl>
+        </div>
 
-        <dl
+        <div
             v-for="item in pools[store.fullPool].variants"
             :key="item.locId"
             class="full-pool-list"
         >
-            <dt class="roboto-flex seven">{{ item.desc }}</dt>
-            <dd class="roboto-flex four">Length: {{ item.length }} ft</dd>
-            <dd class="roboto-flex four">Width: {{ item.width }} ft</dd>
-            <dd class="roboto-flex four">Depth: {{ item.depth }} ft</dd>
-            <dd class="roboto-flex four">
-                <span v-if="item.accessible">Handicap access: yes</span>
-                <span v-if="!item.accessible">Handicap access: no</span>
-            </dd>
-        </dl>
+            <dl>
+                <dt>
+                    <div class="roboto-flex seven">{{ item.desc }}</div>
+                </dt>
+                <dd>
+                    <div class="roboto-flex four">Length: {{ item.length }} ft</div>
+                </dd>
+                <dd>
+                    <div class="roboto-flex four">Width: {{ item.width }} ft</div>
+                </dd>
+                <dd>
+                    <div class="roboto-flex four">Depth: {{ item.depth }} ft</div>
+                </dd>
+                <dd>
+                    <div class="roboto-flex four">
+                        <span v-if="item.accessible">Handicap access: yes</span>
+                        <span v-if="!item.accessible">Handicap access: no</span>
+                    </div>
+                </dd>
+            </dl>
+        </div>
     </div>
 </template>
 
